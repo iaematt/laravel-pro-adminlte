95e1d68d49058c47da9dece174e105c7d5099a34
Matheus Bastos <matheusbastos@outlook.com>
2025-03-18 18:38:50 -0300

    s5e53

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>AdminLTE 4 | Register Page</title>
+
+        @vite('resources/scss/app.scss')
+    </head>
+
+    <body class="register-page bg-body-secondary">
+        @yield('content')
+
+        @vite('resources/js/app.js')
+
+        <script>
+            const SELECTOR_SIDEBAR_WRAPPER = ".sidebar-wrapper";
+            const Default = {
+                scrollbarTheme: "os-theme-light",
+                scrollbarAutoHide: "leave",
+                scrollbarClickScroll: true,
+            };
+            document.addEventListener("DOMContentLoaded", function () {
+                const sidebarWrapper = document.querySelector(SELECTOR_SIDEBAR_WRAPPER);
+                if (sidebarWrapper && typeof OverlayScrollbarsGlobal?.OverlayScrollbars !== "undefined") {
+                    OverlayScrollbarsGlobal.OverlayScrollbars(sidebarWrapper, {
+                        scrollbars: {
+                            theme: Default.scrollbarTheme,
+                            autoHide: Default.scrollbarAutoHide,
+                            clickScroll: Default.scrollbarClickScroll,
+                        },
+                    });
+                }
+            });
+        </script>
+    </body>
+</html>
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <title>AdminLTE v4 | Dashboard</title>
+
+        @vite('resources/scss/app.scss')
+    </head>
+
+    <body class="layout-fixed sidebar-expand-lg bg-body-tertiary">
+        <div class="app-wrapper">
+            @include('fragments/header')
+
+            @include('fragments/sidebar')
+
+            <main class="app-main">
+                @include('fragments/content-header')
+                <div class="app-content">
+                    <div class="container-fluid">
+                        @yield('content')
+                    </div>
+                </div>
+            </main>
+
+            @include('fragments/footer')
+        </div>
+
+        @vite('resources/js/app.js')
+    </body>
+</html>
